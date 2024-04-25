2b24e963a9e25c8d6373263a1242096f7460d429
Briskless <briskless0@gmail.com>
2024-04-25 13:40:58 -0600

    Created LocalData class

---
+++ LocalData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less_Healing
+{
+    public class LocalData
+    {
+        public int lastSavedHealth { get; set; } = 5;
+    }
+}
